4b7c19c3b8d33db4b7fcbffd99fa69755f4017ac
RAYYY <111544394+Glutinfree@users.noreply.github.com>
2024-02-27 18:41:54 -0800

    works

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "../include/json_fwd.hpp"
 #include "myincludes/texturedButton.hpp"
 #include "myincludes/toastHandler.hpp"
+#include "myincludes/database.hpp"
 #include <iostream>
 #include <fstream>
 
@@ -15,7 +16,7 @@ int main() {
     window.SetTargetFPS(240);
     QrCodeScanner qrScanner;
     SCENES currentScene = SCANNING;
-
+    Database database;
     // set up display + constant scene stuff
     SizeScaling::init();
     // set up tabs at top of screen
@@ -24,6 +25,7 @@ int main() {
     Button main(0.0, 0.0, RAYWHITE, BLACK, DARKGRAY, EzText(raylib::Text(GetFontDefault(), "Main Tab"), RAYWHITE, 20.0_spD, 0.0));
     Button dataVisualization(0.0, 0.0, RAYWHITE, BLACK, DARKGRAY, EzText(raylib::Text(GetFontDefault(), "Data Visualization"), RAYWHITE, 20.0_spD, 0.0));
     Button matchConfiguration(0.0,0.0, RAYWHITE, BLACK, DARKGRAY, EzText(raylib::Text(GetFontDefault(), "Match Config"), RAYWHITE, 20.0_spD, 0.0));
+
     tabs.add(&main)
         .add(&dataVisualization)
         .add(&matchConfiguration);
@@ -33,10 +35,14 @@ int main() {
     // __ Scanner Scene __
     Empty scannerScreen(raylib::Rectangle(0,GetScreenHeight() * 0.15, GetScreenWidth(), GetScreenHeight() * 0.85));
     Button goated(400.0_spX, 200.0_spY, RAYWHITE, BLACK, DARKGRAY, EzText(raylib::Text(GetFontDefault(), "Submit"), RAYWHITE, 40.0_spD, 0.0_spX));
+    Button DB(200.0_spX,100.0_spY, RAYWHITE, BLACK, DARKGRAY, EzText(raylib::Text(GetFontDefault(), "BD"), RAYWHITE, 20.0_spD, 0.0));
+
     //std::string fileName = "resources/submit.png";
    // TexturedButton goated(400.0_spX, 200.0_spY, raylib::Image(fileName), raylib::Image(fileName));
     goated.setDisplayPos(BOTTOMCENTERED);
+    DB.setDisplayPos(BOTTOMLEFT);
     scannerScreen.add(&goated);
+    scannerScreen.add(&DB);
 
 
     while(!window.ShouldClose()) {
@@ -56,6 +62,9 @@ int main() {
                 qrScanner.scan();
                 if (goated.isPressed()) {
                     qrScanner.update();
+                }
+                if(DB.isPressed()) {
+                    database.innit();
                 }
                 // drawing
                 window.BeginDrawing();
