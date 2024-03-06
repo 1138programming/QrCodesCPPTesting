6ff108cb841320c113c6f5794d5fc9a1d9e094f5
RAYYY <111544394+Glutinfree@users.noreply.github.com>
2024-03-05 19:25:38 -0800

    cool stuff

--- src/myincludes/database.hpp
+++ src/myincludes/database.hpp
@@ -4,25 +4,64 @@
 #include "../include/mysql/mysql.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "toastHandler.hpp"
 
 class Database {
+    private:
+        MYSQL mysql; 
     public:
-        int innit() { 
-            MYSQL mysql; 
+   
+        Database() { 
+            
 
             if(mysql_init(&mysql)==NULL) { 
                 printf("\nInitialization error\n"); 
-                return -1; 
+                mysql_close(&mysql);
+                toastHandler::add(Toast("fuck you",LENGTH_NORMAL));
+         
             } 
+         
 
-            mysql_real_connect(&mysql,"localhost","root","1138","1138scapp",0,NULL,0); 
+            if (!mysql_real_connect(&mysql,"localhost","root","1138","1138scapp",0,NULL,0)) {
+                fprintf(stderr, "Error connecting to Server: %s\n", mysql_error(&mysql));
+                mysql_close(&mysql);
+                toastHandler::add(Toast("fuck you",LENGTH_NORMAL));
+        
+            }; 
 
             printf("Client version: %s",mysql_get_client_info()); 
             printf("\nServer version: %s",mysql_get_server_info(&mysql)); 
 
 
+      
+                                        
+        }
+
+        MYSQL_RES& execQuery(std::string statement) {
+           
+        
+            if (mysql_query(&mysql, statement.c_str())) {
+                std::cout << "Error executing Query" << mysql_error(&mysql) << std::endl;
+                MYSQL_RES val = (MYSQL_RES){};
+                return val;
+            }
+ 
+            MYSQL_RES *resptr = mysql_use_result(&mysql);
+            if (mysql.status == MYSQL_STATUS_USE_RESULT) {
+                 std::cout << "Error bad" << mysql_error(&mysql) << std::endl;
+            }
+
+            if (resptr == 0) {
+                std::cout << "Error result is zero" << mysql_error(&mysql) << std::endl;
+                MYSQL_RES val = (MYSQL_RES){};
+                return val;
+            }
+            return *resptr;
+            
+        }
+
+        ~Database() {
             mysql_close(&mysql); 
-            return 0; 
         }
 };
 #endif
